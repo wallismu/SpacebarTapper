c28b13e2bf6f35e8efa84177f9a440cc2f54ce17
root <root@ip-172-31-46-112.ec2.internal>
2019-02-26 19:30:03 +0000

    Indended on server

--- SpacebarTapper.js
+++ SpacebarTapper.js
@@ -1,109 +1,109 @@
 class SpacebarTapper {
 	constructor() {
-		this.regions = [0]
+        this.regions = [0]
 
-		this.wavesurfer = WaveSurfer.create({
-		    container: '#waveform',
-	        plugins: [
-	            WaveSurfer.regions.create({})
-	        ]
-		});
+        this.wavesurfer = WaveSurfer.create({
+            container: '#waveform',
+            plugins: [
+                WaveSurfer.regions.create({})
+            ]
+        });
 
-		this.wavesurfer.load('english.mp3');
+        this.wavesurfer.load('english.mp3');
 
-		this.wavesurfer.on('ready', function() {
-	        var st = new window.soundtouch.SoundTouch(
-	            this.wavesurfer.backend.ac.sampleRate
-	        );
-	        var buffer = this.wavesurfer.backend.buffer;
-	        var channels = buffer.numberOfChannels;
-	        var l = buffer.getChannelData(0);
-	        var r = channels > 1 ? buffer.getChannelData(1) : l;
-	        var length = buffer.length;
-	        var seekingPos = null;
-	        var seekingDiff = 0;
+        this.wavesurfer.on('ready', function() {
+            var st = new window.soundtouch.SoundTouch(
+                this.wavesurfer.backend.ac.sampleRate
+            );
+            var buffer = this.wavesurfer.backend.buffer;
+            var channels = buffer.numberOfChannels;
+            var l = buffer.getChannelData(0);
+            var r = channels > 1 ? buffer.getChannelData(1) : l;
+            var length = buffer.length;
+            var seekingPos = null;
+            var seekingDiff = 0;
 
-	        var source = {
-	            extract: function(target, numFrames, position) {
-	                if (seekingPos != null) {
-	                    seekingDiff = seekingPos - position;
-	                    seekingPos = null;
-	                }
+            var source = {
+                extract: function(target, numFrames, position) {
+                    if (seekingPos != null) {
+                        seekingDiff = seekingPos - position;
+                        seekingPos = null;
+                    }
 
-	                position += seekingDiff;
+                    position += seekingDiff;
 
-	                for (var i = 0; i < numFrames; i++) {
-	                    target[i * 2] = l[i + position];
-	                    target[i * 2 + 1] = r[i + position];
-	                }
+                    for (var i = 0; i < numFrames; i++) {
+                        target[i * 2] = l[i + position];
+                        target[i * 2 + 1] = r[i + position];
+                    }
 
-	                return Math.min(numFrames, length - position);
-	            }
-	        }
+                    return Math.min(numFrames, length - position);
+                }
+            }
 
-	        var soundtouchNode;
+            var soundtouchNode;
 
-	        this.wavesurfer.on('play', function() {
-	            seekingPos = ~~(this.wavesurfer.backend.getPlayedPercents() * length);
-	            st.tempo = this.wavesurfer.getPlaybackRate();
+            this.wavesurfer.on('play', function() {
+                seekingPos = ~~(this.wavesurfer.backend.getPlayedPercents() * length);
+                st.tempo = this.wavesurfer.getPlaybackRate();
 
-	            if (st.tempo === 1) {
-	                this.wavesurfer.backend.disconnectFilters();
-	            } else {
-	                if (!soundtouchNode) {
-	                    var filter = new window.soundtouch.SimpleFilter(source, st);
-	                    soundtouchNode = window.soundtouch.getWebAudioNode(
-	                        this.wavesurfer.backend.ac,
-	                        filter
-	                    );
-	                }
-	                this.wavesurfer.backend.setFilter(soundtouchNode);
-	            }
-	        }.bind(this));
+                if (st.tempo === 1) {
+                    this.wavesurfer.backend.disconnectFilters();
+                } else {
+                    if (!soundtouchNode) {
+                        var filter = new window.soundtouch.SimpleFilter(source, st);
+                        soundtouchNode = window.soundtouch.getWebAudioNode(
+                            this.wavesurfer.backend.ac,
+                            filter
+                        );
+                    }
+                    this.wavesurfer.backend.setFilter(soundtouchNode);
+                }
+            }.bind(this));
 
-	        this.wavesurfer.on('pause', function() {
-	            soundtouchNode && soundtouchNode.disconnect();
-	        }.bind(this));
+            this.wavesurfer.on('pause', function() {
+                soundtouchNode && soundtouchNode.disconnect();
+            }.bind(this));
 
-	        this.wavesurfer.on('seek', function() {
-	            seekingPos = ~~(this.wavesurfer.backend.getPlayedPercents() * length);
-	        }.bind(this));
-	    }.bind(this));
+            this.wavesurfer.on('seek', function() {
+                seekingPos = ~~(this.wavesurfer.backend.getPlayedPercents() * length);
+            }.bind(this));
+        }.bind(this));
 
-	}
+    }
 
-	playPause () {
-		this.wavesurfer.playPause();
-	}
+    playPause () {
+        this.wavesurfer.playPause();
+    }
 
-	setPlaybackRate (x) {
-		this.wavesurfer.setPlaybackRate(x);
-	}
+    setPlaybackRate (x) {
+        this.wavesurfer.setPlaybackRate(x);
+    }
 
-	makeARegion() {
-		console.log("making a range");
-		let s = this.regions[this.regions.length-1];
-		let e = this.wavesurfer.getCurrentTime();
-		this.regions.push(e);
-		this.wavesurfer.addRegion({
-			start: s,
-			end: e,
-			color: this.getRandomColor()
-		});
-	}
+    makeARegion() {
+        console.log("making a range");
+        let s = this.regions[this.regions.length-1];
+        let e = this.wavesurfer.getCurrentTime();
+        this.regions.push(e);
+        this.wavesurfer.addRegion({
+            start: s,
+            end: e,
+            color: this.getRandomColor()
+        });
+    }
 
-	getRandomColor() {
-	  /*var letters = '0123456789ABCDEF';
-	  var color = '#';
-	  for (var i = 0; i < 6; i++) {
-	    color += letters[Math.floor(Math.random() * 16)];
-	  }
-	  return color;*/
-	  let r = Math.floor(Math.random() * 256);
-	  let g = Math.floor(Math.random() * 256);
-	  let b = Math.floor(Math.random() * 256);
+    getRandomColor() {
+        /*var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;*/
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
 
-	  return('rgba(' + r + ', ' + g + ', ' + b + ', 0.5)');
-	}
+        return('rgba(' + r + ', ' + g + ', ' + b + ', 0.5)');
+    }
 
 }
--- main.js
+++ main.js
@@ -1,50 +1,50 @@
 times = []
 $(document).ready(function(){
-	console.log("Page loaded");
+    console.log("Page loaded");
 
-	st = new SpacebarTapper();
-	// Don't mangle audio when changing playback speed
-	// Sounds choppy but mostly works
+    st = new SpacebarTapper();
+    // Don't mangle audio when changing playback speed
+    // Sounds choppy but mostly works
 
 
-	$("#playpause").click(function () {
-		console.log("pause/play");
-		st.playPause();
-	});
+    $("#playpause").click(function () {
+        console.log("pause/play");
+        st.playPause();
+    });
 
-	$("#full").click(function () {
-		console.log("full");
-		st.setPlaybackRate(1);
-	});
+    $("#full").click(function () {
+        console.log("full");
+        st.setPlaybackRate(1);
+    });
 
-	$("#half").click(function () {
-		console.log("half");
-		st.setPlaybackRate(.5);
-	});
+    $("#half").click(function () {
+        console.log("half");
+        st.setPlaybackRate(.5);
+    });
 
-	$("#quarter").click(function () {
-		console.log("half");
-		st.setPlaybackRate(.25);
-	});
+    $("#quarter").click(function () {
+        console.log("half");
+        st.setPlaybackRate(.25);
+    });
 
-	document.body.onkeyup = function(e){
-    if(e.keyCode == 77){
-        console.log("M pressed");
-        //makeARegion(wavesurfer, wavesurfer.getCurrentTime());
-        st.makeARegion();
-    	}
-	}
+    document.body.onkeyup = function(e){
+        if(e.keyCode == 77){
+            console.log("M pressed");
+            //makeARegion(wavesurfer, wavesurfer.getCurrentTime());
+            st.makeARegion();
+        }
+    }
 })
 
 makeARegion = function(ws, time) {
-	console.log(time);
-	console.log("MAKING A REGION!!");
-	if (times.length == 0) {
-		ws.addRegion("poop", 0, time, false, true, true, "#ffffff");
-	}
-	else {
-		times.push(time);
-		ws.addRegion("poop", times[times.length-2], times[times.length-1], false, true, true, "#ffffff");
-	}
-	console.log(ws.regions.list.length);
+    console.log(time);
+    console.log("MAKING A REGION!!");
+    if (times.length == 0) {
+        ws.addRegion("poop", 0, time, false, true, true, "#ffffff");
+    }
+    else {
+        times.push(time);
+        ws.addRegion("poop", times[times.length-2], times[times.length-1], false, true, true, "#ffffff");
+    }
+    console.log(ws.regions.list.length);
 }
